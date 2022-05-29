b884a4326e7c55f4e044761f12ea69e0cf920224
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-05-29 15:31:22 +0530

    Time: 583 ms (7.86%), Space: 22.9 MB (34.38%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
-        static bool comp(const vector<int>&first,vector<int>&second)
-    {
-        return first[1]<second[1];
-    }
+    //     static bool comp(const vector<int>&first,vector<int>&second)
+    // {
+    //         //sorting based on the second element 
+    //     return first[1]<second[1];
+    // }
     int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(),pairs.end(),comp);
-        int ans=0;
+        // sort(pairs.begin(),pairs.end(),comp);
+        // int ans=0;
+        // for(int i=0;i<pairs.size();i++)
+        // {
+        //     int count=1;
+        //     int last=pairs[i][1];
+        //     // sorted based on the second element
+        //     for(int j=i+1;j<pairs.size();j++)
+        //     {
+        //         if(last<pairs[j][0])
+        //         {
+        //             count++;
+        //             last=pairs[j][1];
+        //         }
+        //     }
+        //     ans=max(ans,count);
+        // }
+        // return ans;
+        sort(pairs.begin(),pairs.end());
+        vector<int>dp(pairs.size(),1);
+        int ans=1;
         for(int i=0;i<pairs.size();i++)
         {
-            int count=1;
-            int last=pairs[i][1];
             for(int j=i+1;j<pairs.size();j++)
             {
-                if(last<pairs[j][0])
+                if(pairs[i][1]<pairs[j][0])
                 {
-                    count++;
-                    last=pairs[j][1];
+                    dp[j]=max(dp[j],dp[i]+1);
+                    ans=max(ans,dp[j]);
                 }
             }
-            ans=max(ans,count);
         }
         return ans;
     }
