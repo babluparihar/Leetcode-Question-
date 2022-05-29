6f48875bcc7b6555d62a6ca52c8616f2afd8bf15
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-05-29 13:37:02 +0530

    Time: 105 ms (41.00%), Space: 16.4 MB (49.36%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,32 +1,69 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-           int ans=0;
-        for(int i=0;i<words.size();i++)
+//     this is the way of this brutte force 
+        
+        //            int ans=0;
+//         for(int i=0;i<words.size();i++)
 
+//         {
+//             string s=words[i];
+//             unordered_map<char,int>m;
+//             for(auto j:s)
+//             {
+//                 m[j]++;
+//             }
+//             for(int j=i+1;j<words.size();j++)
+//             {
+//                 string temp=words[j];
+//                 bool flag=true;
+//                 for(auto x:temp)
+//                 {
+//                     if(m.find(x)!=m.end())
+//                     {
+//                         flag=false;
+//                         break;
+//                     }
+//                 }
+//                 if(flag==true)
+//                 {
+//                     int a=s.size();
+//                     int b=temp.size();
+//                     ans=max(ans,a*b);
+//                 }
+//             }
+//         }
+//         return ans;
+        
+        // using bitmanipultion
+        
+        int ans=0;
+        for(int i=0;i<words.size();i++)
         {
+            int bit=0;
             string s=words[i];
-            unordered_map<char,int>m;
-            for(auto j:s)
+            for(int j=0;j<s.size();j++)
             {
-                m[j]++;
+                int val=s[j]-'a';
+                bit|=(1<<val);
             }
             for(int j=i+1;j<words.size();j++)
             {
-                string temp=words[j];
                 bool flag=true;
-                for(auto x:temp)
+                for(auto k:words[j])
                 {
-                    if(m.find(x)!=m.end())
+                    int val=k-'a';
+                    int l=bit & (1<<val);
+                    if(l>0)
                     {
                         flag=false;
                         break;
                     }
                 }
-                if(flag==true)
+                if(flag)
                 {
-                    int a=s.size();
-                    int b=temp.size();
+                    int a=words[i].size();
+                    int b=words[j].size();
                     ans=max(ans,a*b);
                 }
             }
