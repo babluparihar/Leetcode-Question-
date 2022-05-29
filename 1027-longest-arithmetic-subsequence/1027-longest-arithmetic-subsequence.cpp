952311633d9750d60a967ee946b275bc121950d2
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-05-30 01:49:06 +0530

    Time: 1212 ms (11.64%), Space: 102.4 MB (73.61%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int longestArithSeqLength(vector<int>& A) {
-     int n = A.size();
-        int result = 0;
-        vector<unordered_map<int, int>> dp(n);
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                int diff = A[i]-A[j];
-                dp[i][diff] = dp[j].count(diff) > 0 ? dp[j][diff] + 1 : 2;
-                result = max(result, dp[i][diff]);
-            }
-        }
-        return result;
+   int n = A.size();
+	assert(n>=1 && n<=1000);
+	if (n < 3) return n;
+	vector<vector<int> > dp(n, vector<int>(n, -1));
+	map<int, int> pos;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			dp[i][j] = 2;
+			int dif = A[j] - A[i];
+			int need = 2 * A[i] - A[j];
+			if (pos.count(need) == 0) continue;
+			dp[i][j] = max(dp[i][j], dp[pos[need]][i] + 1);
+		}
+
+		pos[A[i]] = i;
+	}
+
+	int ans = 2;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			ans = max(ans, dp[i][j]);
+		}
+	}
+
+	return ans;
     }
 };
