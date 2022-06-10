67c66e408addbf4b7b5e90bf13f4fff305459b17
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-11 03:59:01 +0530

    Time: 33 ms (27.82%), Space: 16.5 MB (24.01%) - LeetHub

---
+++ 877-stone-game/877-stone-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int solve(vector<int>&piles,int start,int end)
+    {
+        // base case
+         if(start>end)
+         {
+             return 0;
+         }
+        if(dp[start][end]!=-1)
+        {
+            return dp[start][end];
+        }
+        // taking from the last
+        int a=piles[end]+max(solve(piles,start,end-2),solve(piles,start+1,end-1));
+        int b =piles[start]+max(solve(piles,start+2,end),solve(piles,start+1,end-1));
+        return dp[start][end]=max(a,b);
+    }
+    bool stoneGame(vector<int>& piles) {
+        dp.resize(piles.size(),vector<int>(piles.size(),-1));
+        int x=solve(piles,0,piles.size()-1);
+        int sum=accumulate(piles.begin(),piles.end(),0);
+        sum-=x;
+        return x>=sum;
+    }
+};
