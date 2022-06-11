948662c5181e97a85a4b381366f6d906b7bc3673
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-12 02:35:14 +0530

    Time: 691 ms (20.30%), Space: 127.7 MB (40.47%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -26,55 +26,58 @@ public:
     //     return dp[start][end]=min(a,b);
     // }
     int minOperations(vector<int>& nums, int x) {
-         int p=accumulate(nums.begin(),nums.end(),0);
-            if(p<x)
-            {
-                return -1;
-            }
-            vector<long long int>first,second;
-        long long int sum=0;
-        int ans=INT_MAX;
+        int n=nums.size();
+        int s=accumulate(nums.begin(),nums.end(),0);
+        if(s<x)
+        {
+            return -1;
+        }
+        vector<long long int>first;
+        long long sum=0;
+        int  ans=INT_MAX;
         for(int i=0;i<nums.size();i++)
         {
-            sum+=nums[i];
-            first.push_back(sum);
-            if(sum==x)
-            {
-                ans=min(ans,i+1);
-            }
+        sum+=nums[i];
+        first.push_back(sum);
+        if(sum==x)
+        {
+            ans=min(ans,i+1);
+        }
         }
         sum=0;
+        vector<long long int>second(nums.size());
         for(int i=nums.size()-1;i>=0;i--)
         {
-            sum+=nums[i];
-            second.push_back(sum);
-            if(sum==x)
-            {
-                int n=nums.size();
-                ans=min(ans,n-i);
-            }
+        sum+=nums[i];
+        second[i]=sum;
+        if(sum==x)
+        {
+            ans=min(ans,n-i);
         }
-        reverse(second.begin(),second.end());
+        }
+        
+        
+        
         for(int i=0;i<first.size();i++)
         {
-            long long int temp=x-first[i];
-            long long int t=lower_bound(second.begin(),second.end(),temp)-second.begin();
-            if(t<second.size() and second[t]==temp)
+            int temp=x-first[i];
+            int t=lower_bound(second.begin(),second.end(),temp)-second.begin();
+            if(t<n and second[t]==temp)
             {
-                int value=i+1+second.size()-t-1;
-                ans=min(ans,value);
+                    ans=min(ans,i+1+n-t);
             }
         }
           for(int i=0;i<first.size();i++)
         {
-            long long int temp=x-second[i];
-            long long int t=lower_bound(first.begin(),first.end(),temp)-first.begin();
-            if(t<first.size() and first[t]==temp)
+            int temp=x-second[i];
+            int t=lower_bound(first.begin(),first.end(),temp)-first.begin();
+            if(t<n and first[t]==temp)
             {
-                int value=first.size()-i+t+1;
-                ans=min(ans,value);
+                    int ss=n-i;
+                    int ff=t+1;
+                    ans=min(ans,ss+ff);
             }
         }
-        return ans==INT_MAX?-1:ans;
+        return ans==INT_MAX ?-1:ans;
     }
 };
