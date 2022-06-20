c5b18f3860fd34a88b7e686e8b1fa0d58ebbcdc7
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-20 14:50:48 +0530

    Time: 131 ms (45.05%), Space: 37.2 MB (50.00%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool comp(const string &a,const string &b)
+    {
+        return a.size()>b.size();
+    }
+    int minimumLengthEncoding(vector<string>& words) {
+        int ans=0;
+        unordered_map<string,int>m;
+        sort(words.begin(),words.end(),comp);
+        for(int i=0;i<words.size();i++)
+        {
+            string t=words[i];
+            if(m.find(t)!=m.end())
+            {
+                continue;
+            }
+            for(int j=0;j<t.size();j++)
+            {
+                string s=t.substr(j);
+                m[s]++;
+            }
+            ans+=(words[i].size()+1);
+        }
+        return ans;
+    }
+};
