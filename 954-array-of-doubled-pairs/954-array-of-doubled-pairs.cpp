6517bb4f92b22479cb0900aba3f87986bffa1d15
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-21 10:19:02 +0530

    Time: 133 ms (79.88%), Space: 57.9 MB (80.18%) - LeetHub

---
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        unordered_map<int,int>m;
+        int z=count(arr.begin(),arr.end(),0);
+        if(z%2!=0)
+        {
+            return false;
+        }
+        for(auto i:arr)
+        {
+            m[i]++;
+        }
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            int x=arr[i];
+            if(m.find(x)!=m.end() and m.find(2*x)!=m.end())
+            {
+                count++;
+                m[x]--;
+                m[2*x]--;
+                if(m[x]==0)
+                {
+                    m.erase(x);
+                }
+                if(m[2*x]==0)
+                {
+                    m.erase(2*x);
+                }
+            }
+        }
+        return count==arr.size()/2;
+    }
+};
