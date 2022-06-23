e4e0b2d11b9c1c6b967e23d881ebbec6e8e4ad68
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-23 14:01:54 +0530

    Time: 714 ms (60.77%), Space: 131.8 MB (87.80%) - LeetHub

---
+++ 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int maximumScore(vector<int>& scores, vector<vector<int>>& edges) {
+        int n=scores.size();
+        vector<int>adj[n];
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        for(auto &l:adj)
+        {
+            // sort(adj[i].begin(),adj[i].end(),[&](const int &a,const int &b){return scores[a]>scores[b];});
+             partial_sort(begin(l), begin(l) + min((int)l.size(), 3), end(l), [&](int i, int j){ return scores[i] > scores[j]; });
+        l.resize(min((int)l.size(), 3));
+        }
+        int res=-1;
+        for(auto &e:edges)
+        {
+            for(int &i:adj[e[0]])
+            {
+                for(int &j:adj[e[1]])
+                {
+                    if(i!=j and i!=e[1] and j!=e[0])
+                    {
+                        int temp=scores[i]+scores[j]+scores[e[0]]+scores[e[1]];
+                        res=max(res,temp);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
