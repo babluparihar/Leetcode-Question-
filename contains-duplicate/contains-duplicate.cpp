6e4ac142342928fee068de27ad7f51fae4a5c319
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-25 13:39:46 +0530

    Time: 203 ms, Memory: 51.3 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(auto i:nums)
+        {
+            m[i]++;
+            if(m[i]==2){return true;}
+        }
+        return false;
+    }
+};
