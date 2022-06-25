c13c1edcbdedaee13d573a6933aa608e26e88fb8
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-25 16:12:38 +0530

    Time: 4 ms, Memory: 10.3 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    vector<int>ans;
+    vector<int>nums1;
+    vector<int>nums2;
+    void solve(int i,int j)
+    {
+        if(i>=nums1.size() or j>=nums2.size())
+        {
+            return;
+        }
+        if(nums1[i]==nums2[j])
+        {
+            ans.push_back(nums1[i]);
+            solve(i+1,j+1);
+        }
+        else if(nums1[i]>nums2[j])
+        {
+            solve(i,j+1);
+        }
+        else
+        {
+            solve(i+1,j);
+        }
+    }
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         // first solution using the unordered_map
         /*
@@ -24,33 +47,44 @@ public:
         return ans; */
         
         //second solution using the sorting technique
+        // we can solve this question using the recursion and i want to solve this question using recursion
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        int i=0;
-        int j=0;
-        vector<int>nums;
-        while(true)
-        {
-            // go by minimum element
-            while(i<nums1.size() and j<nums2.size() and nums1[i]==nums2[j])
-            {
-                nums.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            if(i<nums1.size() and j<nums2.size() and nums1[i]>nums2[j])
-            {
-                j++;
-            }
-            else
-            {
-               i++;
-            }
-            if(i>=nums1.size() or j>=nums2.size())
-            {
-                return nums;
-            }
-        }
-        return nums;
+        
+        
+        // iterative version
+        
+        // int i=0;
+        // int j=0;
+        // vector<int>nums;
+        // while(true)
+        // {
+        //     // go by minimum element
+        //     while(i<nums1.size() and j<nums2.size() and nums1[i]==nums2[j])
+        //     {
+        //         nums.push_back(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        //     if(i<nums1.size() and j<nums2.size() and nums1[i]>nums2[j])
+        //     {
+        //         j++;
+        //     }
+        //     else
+        //     {
+        //        i++;
+        //     }
+        //     if(i>=nums1.size() or j>=nums2.size())
+        //     {
+        //         return nums;
+        //     }
+        // }
+        // return nums;
+        
+        //recursive version
+        this->nums1=nums1;
+        this->nums2=nums2;
+        solve(0,0);
+        return ans;
     }
 };
