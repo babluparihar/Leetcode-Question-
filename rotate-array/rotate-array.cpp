6ee7deab45176ff423b2de47dfcb8422fd8b1b2b
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-25 13:31:07 +0530

    Time: 57 ms, Memory: 25.7 MB - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -6,26 +6,39 @@ public:
         
         // first solution
         
-        if(k==0)
-        {
-            return ;
-        }
-        else
-        {
-            int i=0;
-            int j=nums.size()-1;
+        // if(k==0)
+        // {
+        //     return ;
+        // }
+        // else
+        // {
+        //     int i=0;
+        //     int j=nums.size()-1;
             k=k%nums.size();
-            int t=k;
-            while(i<=j and t--)
-            {
-                swap(nums[i],nums[j]);
-                i++;
-                j--;
-            }
-            reverse(nums.begin(),nums.begin()+k);
-            reverse(nums.begin()+k,nums.end());
-            reverse(nums.begin()+2*k,nums.end());
-        }
+        //     int t=k;
+        //     while(i<=j and t--)
+        //     {
+        //         swap(nums[i],nums[j]);
+        //         i++;
+        //         j--;
+        //     }
+        //     reverse(nums.begin(),nums.begin()+k);
+        //     reverse(nums.begin()+k,nums.end());
+        //     reverse(nums.begin()+2*k,nums.end());
+        // }
         
+        //second solution using space
+        
+        vector<int>temp(nums);
+        int t=nums.size()-k;
+        for(int i=0;i<nums.size() and t<nums.size();i++)
+        {
+            nums[i]=temp[t++];
+        }
+        t=0;
+        for(int i=k;i<nums.size();i++)
+        {
+            nums[i]=temp[t++];
+        }
     }
 };
