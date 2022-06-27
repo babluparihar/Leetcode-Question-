e46a4774d1ce3f394072bdf2834d18748396648d
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-27 18:10:04 +0530

    Time: 71 ms (82.46%), Space: 17.8 MB (40.69%) - LeetHub

---
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& ele) {
+        vector<string>ans;
+        unordered_map<char,int>m;
+        for(int i=0;i<s.size();i++)
+        {
+            m[s[i]]++;
+        }
+        for(int ii=0;ii<ele.size();ii++)
+        {
+            string temp=ele[ii];
+            if(temp.size()<=s.size())
+            {
+                int i=0;
+                int j=0;
+                while(i<s.size())
+                {
+                    if(s[i]==temp[j])
+                    {
+                        i++;
+                        j++;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                    if(j==temp.size())
+                    {
+                        break;
+                    }
+                }
+                if(j==temp.size())
+                {
+                  ans.push_back(ele[ii]);
+                }
+            }
+        }
+        if(ans.size()==0)
+        {
+            return {};
+        }
+        sort(ans.begin(),ans.end(),[&](const string &a,const string &b){ if(a.size()==b.size()){return a<b;}else{return a.size()>b.size();}});
+        return ans[0];
+    }
+};
