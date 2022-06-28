7c591447a86ba8f76ce19340a13b61b3598eff9a
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-28 10:05:26 +0530

    Time: 57 ms (88.01%), Space: 17.2 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -6,45 +6,65 @@ public:
         {
             vec[i-'a']++;
         }
-        priority_queue<pair<int,char>>q;
-        for(int i=0;i<26;i++)
+        
+        int ans=0
+            ;
+        sort(vec.begin(),vec.end());
+        for(int i=24;i>=0;i--)
         {
-            q.push({vec[i],i+'a'});
-        }
-        
-        // brute force approach 
-        
-        
-        // unordered_map<char,int>m;
-        // for(auto i:s)
-        // {
-        //     m[i]++;
-        // }
-        // priority_queue<pair<int,char>>q;
-        // for(auto i:m)
-        // {
-        //     q.push({i.second,i.first});
-        // }
-        int mx=q.top().first;
-        q.pop();
-        int ans=0;
-        while(!q.empty())
-        {
-            while(q.top().first==mx)
-            {
-                auto x=q.top();
-                x.first--;
-                q.pop();
-                q.push(x);
-                ans++;
-            }
-            mx=q.top().first;
-            q.pop();
-            if(mx<=0)
+             if(vec[i]<=0)
             {
                 break;
             }
+            if(vec[i]>=vec[i+1])
+            {
+                int prev=vec[i];
+                vec[i]=max(0,vec[i+1]-1);
+                ans+=(prev-vec[i]);
+            }
         }
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // // this is using priority queue but we can solve it by using less number of operation
+        // priority_queue<pair<int,char>>q;     // this is sorted on the basis of the  element frequency
+        // for(int i=0;i<26;i++)
+        // {
+        //     q.push({vec[i],i+'a'});
+        // }
+        // int mx=q.top().first;
+        // q.pop();
+        // int ans=0;
+        // while(!q.empty())
+        // {
+        //     while(q.top().first==mx)
+        //     {
+        //         auto x=q.top();
+        //         x.first--;
+        //         q.pop();
+        //         q.push(x);
+        //         ans++;
+        //     }
+        //     mx=q.top().first;
+        //     q.pop();
+        //     if(mx<=0)
+        //     {
+        //         break;
+        //     }
+        // }
+        // return ans;
     }
 };
