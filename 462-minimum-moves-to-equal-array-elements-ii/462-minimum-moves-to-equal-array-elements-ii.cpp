026601a647cf2d0bb158bdee42684c86883805ae
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-06-30 16:20:49 +0530

    Time: 21 ms (26.38%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        
+        // reason behind for this problem is that we just need to add or remove so the minimum operation will be that we equal all elements to the middle element
+        
         int ele=nums[nums.size()/2];
         int ans=0;
         for(auto i:nums)
