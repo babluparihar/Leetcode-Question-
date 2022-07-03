f76a6e8b2cdaa33888ff7b8efe6c63ebe19f393c
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-07-03 20:03:23 +0530

    Time: 4 ms (56.21%), Space: 7.8 MB (6.41%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -47,37 +47,5 @@ public:
         dp.resize(nums.size()+1,vector<int>(2,-1));
         int b=solve(0,true);
         return max(a,b);
-
-        
-        
-        
-        
-        
-        
-//         // need to take two array to get the length for the elements 
-//         // for every elements their is two choice of that element
-//         // I have to search for the solution so I could not solve it by using the methods
-//         vector<int>up(nums.size(),1);
-//         vector<int>down(nums.size(),1);
-//         for(int i=1;i<nums.size();i++)
-//         {
-//             // up position 
-//             if(nums[i]>nums[i-1])
-//             {
-//                 up[i]=down[i-1]+1;
-//                 down[i]=down[i-1];
-//             }
-//             else if(nums[i]<nums[i-1])
-//             {
-//                 down[i]=up[i-1]+1;
-//                 up[i]=up[i-1];
-//             }
-//             else
-//             {
-//                 down[i]=down[i-1];
-//                 up[i]=up[i-1];
-//             }
-//         }
-//         return max(down[nums.size()-1],up[nums.size()-1]);
     }
 };
