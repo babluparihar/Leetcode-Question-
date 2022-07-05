dc77049353f20434ff29a8dbd5fa405302634a97
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-07-05 18:18:43 +0530

    Time: 121 ms (84.98%), Space: 33.8 MB (85.63%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,32 +1,74 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()<=0)
+        if(nums.size()<=1)
         {
             return nums.size();
         }
-        set<int>s;
-        for(auto i:nums)
-        {
-            s.insert(i);
-        }
+        sort(nums.begin(),nums.end());
+        int count=1;
         int ans=0;
-        int count=0;
-        auto last=s.begin();
-        for(auto i=s.begin();i!=s.end();i++)
+        int last=nums[0];
+        for(int i=1;i<nums.size();i++)
         {
-            if(*i-*last<=1)
+           while(i<nums.size() and nums[i]==last)
+           {
+               i++;
+           }
+            if(i<nums.size() and nums[i]-last==1)
             {
-                count++;
+            count++;
             }
             else
             {
-                ans=max(ans,count);
-                count=1;
+            ans=max(ans,count);
+            count=1;
             }
-            last=i;
+            last=nums[i];
         }
         ans=max(ans,count);
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // here I am using the set nlogn so also space is consumed
+        // set<int>s;
+        // for(auto i:nums)
+        // {
+        //     s.insert(i);
+        // }
+        // int ans=0;
+        // int count=0;
+        // auto last=s.begin();
+        // for(auto i=s.begin();i!=s.end();i++)
+        // {
+        //     if(*i-*last<=1)
+        //     {
+        //         count++;
+        //     }
+        //     else
+        //     {
+        //         ans=max(ans,count);
+        //         count=1;
+        //     }
+        //     last=i;
+        // }
+        // ans=max(ans,count);
+        // return ans;
     }
 };
