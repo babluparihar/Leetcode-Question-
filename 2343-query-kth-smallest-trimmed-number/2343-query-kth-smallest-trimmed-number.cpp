a79a5c6bbc84f758bc6933078dded55afd6e25fb
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-07-17 12:09:48 +0530

    Time: 1838 ms (11.11%), Space: 372.4 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -8,20 +8,13 @@ public:
             int k=i[0];
             int len=i[1];
             int start=nums[0].size()-len;
-            priority_queue<pair<string,int>,vector<pair<string,int>>,greater<pair<string,int>>>q;
+            vector<pair<string,int>>q;
             for(int i=0;i<nums.size();i++)
             {
-                string s=nums[i];
-                string x=s.substr(start,len);
-                q.push({x,i});
+                q.push_back({nums[i].substr(start,len),i});
             }
-            int f=0;
-            while(k-- and q.size()!=0)
-            {
-                f=q.top().second;
-                q.pop();
-            }
-            ans.push_back(f);
+            sort(q.begin(),q.end());
+            ans.push_back(q[k-1].second);
         }
         return ans;
     }
