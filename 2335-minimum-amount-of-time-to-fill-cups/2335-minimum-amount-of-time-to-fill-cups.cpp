26b371271dd906649ca075f5c687b1cd20bc2aa2
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-07-19 14:12:06 +0530

    Time: 10 ms (20.57%), Space: 11.8 MB (53.28%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int fillCups(vector<int>& amount) {
-        priority_queue<int>q;
-        for(auto i:amount)
-        {
-            q.push(i);
-        }
-        int ans=0;
-        while(true)
-        {
-            int x=q.top();
-            q.pop();
-            int y=q.top();
-            q.pop();
-            if(y==0)
-            {
-                ans+=x;
-                return ans;
-            }
-            else
-            {
-                x--;
-                y--;
-                ans++;
-                q.push(x);
-                q.push(y);
-            }
-        } 
-        return ans;
+        int sum=accumulate(amount.begin(),amount.end(),0);
+        return max(*max_element(amount.begin(),amount.end()),(sum+1)/2);
+        // priority_queue<int>q;
+        // for(auto i:amount)
+        // {
+        //     q.push(i);
+        // }
+        // int ans=0;
+        // while(true)
+        // {
+        //     int x=q.top();
+        //     q.pop();
+        //     int y=q.top();
+        //     q.pop();
+        //     if(q.top()==0)
+        //     {
+        //         ans+=(x-y);
+        //         return ans;
+        //     }
+        //     else
+        //     {
+        //         int diff=q.top();
+        //         ans+=diff;
+        //         q.push(x-diff);
+        //         q.push(y-diff);
+        //     }
+        // } 
+        // return ans;
     }
 };
