1a83136c0aea6a1f985036be09d5c963e59aded5
Bablu Parihar <66169499+babluparihar@users.noreply.github.com>
2022-07-20 16:37:39 +0530

    Time: 392 ms (48.90%), Space: 38 MB (87.39%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,48 +1,66 @@
 class Solution {
 public:
-    struct trie{
-        trie *child[26];
-        bool isend;
-        trie()
-        {
-            isend=false;
-            for(int i=0;i<26;i++)
-            {
-                child[i]=NULL;
-            }
-        }
-    };
-    void insert(trie *root,string &s)
+    // struct trie{
+    //     trie *child[26];
+    //     bool isend;
+    //     trie()
+    //     {
+    //         isend=false;
+    //         for(int i=0;i<26;i++)
+    //         {
+    //             child[i]=NULL;
+    //         }
+    //     }
+    // };
+    // void insert(trie *root,string &s)
+    // {
+    //     trie *curr=root;
+    //     for(int i=0;i<s.size();i++)
+    //     {
+    //         int ind=s[i]-'a';
+    //         if(curr->child[ind]==NULL)
+    //         {
+    //             curr->child[ind]=new trie();
+    //         }
+    //         curr=curr->child[ind];
+    //     }
+    //     curr->isend=true;
+    // }
+    // bool search(trie *root,string &s,string match)
+    // {
+    //     trie *curr=root;
+    //     int j=0;
+    //     for(int i=0;i<s.size();i++)
+    //     {
+    //         if(s[i]==match[j])
+    //         {
+    //             j++;
+    //         }
+    //         curr=curr->child[s[i]-'a'];
+    //         if(j>=match.size())
+    //         {
+    //             return true;
+    //         }
+    //     }
+    //    return j>=match.size();
+    // }
+    bool search(string &s,string match)
     {
-        trie *curr=root;
-        for(int i=0;i<s.size();i++)
+        if(match.size()>s.size())
         {
-            int ind=s[i]-'a';
-            if(curr->child[ind]==NULL)
-            {
-                curr->child[ind]=new trie();
-            }
-            curr=curr->child[ind];
+            return false;
         }
-        curr->isend=true;
-    }
-    bool search(trie *root,string &s,string match)
-    {
-        trie *curr=root;
+        int i=0;
         int j=0;
-        for(int i=0;i<s.size();i++)
+        while(j<match.size() and i<s.size())
         {
-            if(s[i]==match[j])
+            if(match[j]==s[i])
             {
                 j++;
             }
-            curr=curr->child[s[i]-'a'];
-            if(j>=match.size())
-            {
-                return true;
-            }
+            i++;
         }
-       return j>=match.size();
+        return j>=match.size();
     }
     int numMatchingSubseq(string s, vector<string>& words) {
         unordered_map<string,int>m;
@@ -50,12 +68,12 @@ public:
         {
             m[words[i]]++;
         }
-        trie *root=new trie();
+        // trie *root=new trie();
         int ans=0;
-        insert(root,s);
+        // insert(root,s);
         for(auto i:m)
         {
-            if(search(root,s,i.first))
+            if(search(s,i.first))
             {
                 ans+=i.second;
             }
